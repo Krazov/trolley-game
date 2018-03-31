52992371bb98f4cc1cdf487951bd81afe3f85034
Jon Krazov <krazov@interia.pl>
2018-03-31 22:45:52 +0200

    Adding new messages to trolley.dilemma.messages
    * Moving message-count to this namespace
    * In general, prepending all messages with "---" for legibility (will be
    removed later)
    * Expangin real trolley dilemma with separate message for 0 workers
    * Adding responses to decision about lever and continuing of playing
    * Adjusting summary to simplified structure of data

--- src/trolley_dilemma/messages.cljs
+++ src/trolley_dilemma/messages.cljs
@@ -1,38 +1,69 @@
 (ns trolley-dilemma.messages
     (:require
+        [reagent.core :as r]
         [trolley-dilemma.utils :as u]))
 
 ;; helpers
 (defn plural? [number]
     (if (< 1 (u/absolute number)) "s" ""))
 
+(defonce message-count (r/atom 0))
+(defn generate-message [message]
+    {:id (reset! message-count (inc @message-count)) :message message})
+
 ;; dilemmas
 (defn real-trolley [lowertrack uppertrack]
-    [(str
+    ["---"
+     (str
          "A runaway trolley is barrelling towards "
-         (if (= 1 lowertrack)
-             "one worker who is mysteriously tied up."
-             (str lowertrack " workers who are mysteriously tied up.")))
+         (case lowertrack
+             0 "no workers who would be"
+             1 "one worker who is"
+             (str lowertrack " workers who are"))
+         " mysteriously tied up.")
      (str
          "You can pull a lever to divert the trolley to another track, containting "
-         (if (= 1 uppertrack)
-             "one worker who is also tied up."
-             (str uppertrack " workers who are also tied up.")))])
+         (case uppertrack
+             0 "no workers who would be"
+             1 "one worker who is also"
+             (str uppertrack " workers who are also"))
+         " tied up.")])
 
 ;; decisions
 (defn pull-lever? []
-    ["Do you pull the lever?"])
+    ["---" "Do you pull the lever?"])
+
+(defn lever-pulled []
+    ["---" "You have pulled the lever."])
+
+(defn lever-left []
+    ["---" "You let the lever be."])
 
 (defn keep-playing? []
-    ["Do you want to keep playing?"])
+    ["---" "Do you want to keep playing?"])
+
+(defn keep-playing []
+    ["---" "Alright, let’s keep going."])
+
+(defn stop-playing []
+    ["---" "Life is a series of trolley problems. You cannot avoid them."])
+
+;; consequences
+(defn real-trolley-consequences [dilemma result]
+;    (let
+;        [uppertrack? (> 1 (:uppertrack dilemma))]
+;        (cond
+;            (and uppertrack? ())))
+    )
 
 ;; summary
 (defn summary [data]
     (let
         [rounds (:rounds data)
          points (:points data)
-         kantpoints (:kantpoints points)
-         utils (:utils points)]
-        [(str "You have encountered " rounds " ethical dilemma" (plural? rounds))
+         kantpoints (:kantpoints data)
+         utils (:utils data)]
+        ["---"
+         (str "You have encountered " rounds " ethical dilemma" (plural? rounds))
          (str "You have " kantpoints " kantpoint" (plural? kantpoints))
          (str "You have " utils " util" (plural? utils))]))
