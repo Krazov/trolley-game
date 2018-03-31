6c665871629a3be7e6a814ea3ac5f3b367ed9f3b
Jon Krazov <krazov@interia.pl>
2018-03-31 22:38:58 +0200

    Adding hiccups to project dependencies
    (But commented as there was no use for them so far)

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
 
     :dependencies [[org.clojure/clojure "1.8.0"]
                    [org.clojure/clojurescript "1.9.946"]
+                   ;[hiccups "0.3.0"]
                    [reagent "0.7.0"]]
 
     :plugins [[lein-cljsbuild "1.1.5"]
